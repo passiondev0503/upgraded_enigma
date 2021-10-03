a82db091ac4b6285b2d235e3ca5c4d4c3a503a27
passiondev0503 <passiondev0503@gmail.com>
2021-10-03 07:58:42 -0700

    ci(deploy): update ci deploy jobs, update api app, bump deps (#57)
    - [X] bump project dependencies;
    - [X] update ci deploy jobs - use package scripts, add api ommented deploy;
    - [X] add axios http module to api app;
    - [X] rename github workflows;
    
    BREAKING CHANGE: reinstall project dependencies

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BackendGrpcModule } from '@app/backend-grpc';
 import { API_ENV } from '@app/backend-interfaces';
 import { BackendLoggerMiddleware } from '@app/backend-logger';
 import { BackendWebsocketModule } from '@app/backend-websocket';
+import { HttpModule } from '@nestjs/axios';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { environment } from '../environments/environment';
@@ -11,7 +12,17 @@ import { environment } from '../environments/environment';
  * Root API application module.
  */
 @Module({
-  imports: [BackendDiagnosticsModule.forRoot(), BackendWebsocketModule.forRoot(), BackendGrpcModule.forRoot(environment)],
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        timeout: 10000,
+        maxRedirects: 5,
+      }),
+    }),
+    BackendDiagnosticsModule.forRoot(),
+    BackendWebsocketModule.forRoot(),
+    BackendGrpcModule.forRoot(environment),
+  ],
   providers: [
     {
       provide: API_ENV,
