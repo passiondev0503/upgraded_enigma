ef6b2c53749c7a0c9de505da0496fc4fe2bcdf4f
passiondev0503 <passiondev0503@gmail.com>
2022-05-01 14:51:08 +0300

    test(unit): increase unit test coverage (#163)
    - [x] bump client-store-theme unit coverage to 100%;

--- libs/client-store/jest.config.ts
+++ libs/client-store/jest.config.ts
@@ -8,10 +8,10 @@ const config = {
   coverageThreshold: {
     // TODO: bump unit test coverage and remove this override
     global: {
-      branches: 17,
-      functions: 22,
-      lines: 28,
-      statements: 28,
+      branches: 20,
+      functions: 27.81,
+      lines: 35.54,
+      statements: 35.54,
     },
   },
   preset: '../../jest.preset.ts',
