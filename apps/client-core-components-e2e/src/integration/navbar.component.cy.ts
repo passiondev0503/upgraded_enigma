3c66da34d7795e7f37eab03b1361f2be1f7e7f8c
passiondev0503 <passiondev0503@gmail.com>
2023-01-11 11:57:19 +0200

    test(e2e): add a method to access storybook iframe content (#281)

--- apps/client-core-components-e2e/src/integration/navbar.component.cy.ts
+++ apps/client-core-components-e2e/src/integration/navbar.component.cy.ts
@@ -2,6 +2,12 @@ describe('navbar component', () => {
   beforeEach(() => cy.visit('/?path=/story/appnavbarcomponent--primary'));
 
   it('TODO: test', () => {
-    expect(true).to.eq(true);
+    /**
+     * Gets an iframe content.
+     */
+    const getIframe = () =>
+      cy.get('iframe[id="storybook-preview-iframe"]').its('0.contentDocument.body').should('not.be.empty').then(cy.wrap);
+
+    getIframe().find('app-navbar').should('exist');
   });
 });
--- apps/client-diagnostics-e2e/src/integration/info/info.component.cy.ts
+++ apps/client-diagnostics-e2e/src/integration/info/info.component.cy.ts
@@ -2,6 +2,12 @@ describe('info component', () => {
   beforeEach(() => cy.visit('/?path=/story/appdiagnosticsinfopage--primary'));
 
   it('should render the component', () => {
-    cy.get('app-diagnostics-info-page').should('exist');
+    /**
+     * Gets an iframe content.
+     */
+    const getIframe = () =>
+      cy.get('iframe[id="storybook-preview-iframe"]').its('0.contentDocument.body').should('not.be.empty').then(cy.wrap);
+
+    getIframe().find('app-diagnostics-info-page').should('exist');
   });
 });
